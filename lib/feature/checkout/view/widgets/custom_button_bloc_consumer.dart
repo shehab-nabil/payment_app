9e7e65376ebb2199ac22ed3eed5f4b7446384fd5
shehab nabil <shehabnabil790@gmail.com>
2024-07-14 14:26:49 +0300

    create models for paypal

--- lib/feature/checkout/view/widgets/custom_button_bloc_consumer.dart
+++ lib/feature/checkout/view/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment_app/core/widgets/custom_button.dart';
 import 'package:payment_app/feature/checkout/data/models/payment_intent_input_model.dart';
 import 'package:payment_app/feature/checkout/manager/payment_cubit/payment_cubit.dart';
@@ -33,8 +36,59 @@ class CustomButtonBlocConsumer extends StatelessWidget {
               // var transctionsData = getTransctionsData();
               // exceutePaypalPayment(context, transctionsData);
               // } else {
-              excuteStripePayment(context);
+              // excuteStripePayment(context);
               // }
+
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '100',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '100',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
             },
             isLoading: state is PaymentLoading ? true : false,
             text: 'continue');
@@ -52,3 +106,55 @@ void excuteStripePayment(BuildContext context) {
   BlocProvider.of<PaymentCubit>(context)
       .makePayment(paymentIntentInputModel: paymentIntentInputModel);
 }
+
+// void exceutePaypalPayment(BuildContext context,
+//     ({AmountModel amount, ItemListModel itemList}) transctionsData) {
+//   Navigator.of(context).push(MaterialPageRoute(
+//     builder: (BuildContext context) => PaypalCheckoutView(
+//       sandboxMode: true,
+//       clientId: ApiKeys.clientID,
+//       secretKey: ApiKeys.paypalSecretKey,
+//       transactions: [
+//         {
+//           "amount": transctionsData.amount.toJson(),
+//           "description": "The payment transaction description.",
+//           "item_list": transctionsData.itemList.toJson(),
+//         }
+//       ],
+//       note: "Contact us for any questions on your order.",
+//       onSuccess: (Map params) async {
+//         log("onSuccess: $params");
+//         Navigator.pushAndRemoveUntil(
+//           context,
+//           MaterialPageRoute(builder: (context) {
+//             return const ThankYouView();
+//           }),
+//           (route) {
+//             if (route.settings.name == '/') {
+//               return true;
+//             } else {
+//               return false;
+//             }
+//           },
+//         );
+//       },
+//       onError: (error) {
+//         SnackBar snackBar = SnackBar(content: Text(error.toString()));
+//         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+//         Navigator.pushAndRemoveUntil(
+//           context,
+//           MaterialPageRoute(builder: (context) {
+//             return const MyCartView();
+//           }),
+//           (route) {
+//             return false;
+//           },
+//         );
+//       },
+//       onCancel: () {
+//         print('cancelled:');
+//         Navigator.pop(context);
+//       },
+//     ),
+//   ));
+// }
